b02d0ed688600c6b8fde6297a526d63baccb7ed7
Thetsmr <thetsmr@gmail.com>
2026-09-07 09:32:22 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -40,7 +40,7 @@ use GlpiPlugin\Consumables\Servicecatalog;
 use GlpiPlugin\Consumables\Validation;
 use GlpiPlugin\Servicecatalog\Main;
 
-define('PLUGIN_CONSUMABLES_VERSION', '2.1.8');
+define('PLUGIN_CONSUMABLES_VERSION', '2.1.9');
 
 if (!defined("PLUGIN_CONSUMABLES_DIR")) {
     define("PLUGIN_CONSUMABLES_DIR", Plugin::getPhpDir("consumables"));
